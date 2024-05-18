58a1c514037d0d190e01b5811c6d42f6cced5a35
fullstackbabulal <fullstack.babulal@gmail.com>
2024-05-18 15:14:05 +0530

    API Response class

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,7 @@
+export class ApiResponse{
+  constructor(statusCode, data, message = "Success"){
+    this.statusCode = statusCode
+    this.message = message
+    this.success = statusCode <= 200
+  }
+}
