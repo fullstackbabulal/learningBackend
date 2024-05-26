8580f1dc35ba6cb6344a201ef089320166ee2416
fullstackbabulal <fullstack.babulal@gmail.com>
2024-05-27 00:41:24 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// routes
+// routes Import
 
-import userRouter from "./routes/user.routes.js";
+import {router} from "./routes/user.routes.js";
 
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", router)
